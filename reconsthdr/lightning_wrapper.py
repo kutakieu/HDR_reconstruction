92368c7ad35243544044f23bc439d6c37998b8c8
kutakieu <kutakieu22@gmail.com>
2023-12-19 01:05:05 +0900

    add scale variant mse

--- reconsthdr/lightning_wrapper.py
+++ reconsthdr/lightning_wrapper.py
@@ -3,9 +3,10 @@ from typing import Dict
 import numpy as np
 from lightning import pytorch as pl
 
-from reconsthdr.lossfn_optimizer import optimizer_factory
-from reconsthdr.metrics import metrics_dict
-from reconsthdr.models import loss_fn_factory, model_factory
+from reconsthdr.lossfn_optimizer import (loss_function_factory,
+                                         optimizer_factory)
+# from reconsthdr.metrics import metrics_dict
+from reconsthdr.models import model_factory
 from reconsthdr.utils.logger import get_logger
 
 logger = get_logger(__name__)
@@ -15,7 +16,7 @@ class LightningWrapper(pl.LightningModule):
     def __init__(self, cfg) -> None:
         super().__init__()
         self.net = model_factory(cfg)
-        self.loss_fn = loss_fn_factory(cfg)
+        self.loss_fn = loss_function_factory(cfg.model.lossfn)
         self.optimizer_name = cfg.training.optimizer.name
         self.learning_rate = cfg.training.optimizer.learning_rate
         self.training_step_outputs = []
@@ -29,11 +30,12 @@ class LightningWrapper(pl.LightningModule):
 
     # override
     def training_step(self, batch, batch_idx):
-        pred = self.net(batch)
-        loss = self.loss_fn(pred, batch)
-        self.log("loss_train_step", loss["total"], sync_dist=True)
-        self.training_step_outputs.append({"loss": loss["total"].detach().cpu().numpy()})
-        return loss["total"]
+        gt_hdr, ldr = batch
+        pred_hdr = self.net(ldr)
+        loss = self.loss_fn(pred_hdr, gt_hdr)
+        self.log("loss_train_step", loss, sync_dist=True)
+        self.training_step_outputs.append({"loss": loss.detach().cpu().numpy()})
+        return loss
 
     # override
     def on_train_epoch_end(self) -> None:
@@ -43,38 +45,40 @@ class LightningWrapper(pl.LightningModule):
 
     # override
     def validation_step(self, batch, batch_idx):
-        loss, net_output = self.feed_forward_fn(self.net, batch)
-        metrics = self.calc_metrics(net_output, batch)
-        self.validation_step_outputs.append({"loss": loss["total"].detach().cpu().numpy(), "metrics": metrics})
-        return loss["total"]
+        gt_hdr, ldr = batch
+        pred_hdr = self.net(ldr)
+        loss = self.loss_fn(pred_hdr, gt_hdr)
+        self.validation_step_outputs.append({"loss": loss.detach().cpu().numpy()})
+        return loss
 
     # override
     def on_validation_epoch_end(self) -> None:
         loss_avg = np.stack([x["loss"] for x in self.validation_step_outputs]).mean()
         self.log("loss_val", loss_avg, sync_dist=True)
-        for key in metrics_dict.keys():
-            iou_avg = np.stack([x["metrics"][key] for x in self.validation_step_outputs]).mean()
-            self.log(f"{key}_val", iou_avg, sync_dist=True)
+        # for key in metrics_dict.keys():
+        #     iou_avg = np.stack([x["metrics"][key] for x in self.validation_step_outputs]).mean()
+        #     self.log(f"{key}_val", iou_avg, sync_dist=True)
         self.validation_step_outputs.clear()
 
     # override
     def test_step(self, batch, batch_idx):
-        loss, net_output = self.feed_forward_fn(self.net, batch)
-        metrics = self.calc_metrics(net_output, batch)
-        self.test_step_outputs.append({"loss": loss["total"].detach().cpu().numpy(), "metrics": metrics})
+        gt_hdr, ldr = batch
+        pred_hdr = self.net(ldr)
+        loss = self.loss_fn(pred_hdr, gt_hdr)
+        self.test_step_outputs.append({"loss": loss["total"].detach().cpu().numpy()})
         return loss["total"]
 
     # override
     def on_test_epoch_end(self) -> None:
         loss_avg = np.stack([x["loss"] for x in self.test_step_outputs]).mean()
         self.log("loss_test", loss_avg, sync_dist=True)
-        for key in metrics_dict.keys():
-            iou_avg = np.stack([x["metrics"][key] for x in self.test_step_outputs]).mean()
-            self.log(f"{key}_test", iou_avg, sync_dist=True)
+        # for key in metrics_dict.keys():
+        #     iou_avg = np.stack([x["metrics"][key] for x in self.test_step_outputs]).mean()
+        #     self.log(f"{key}_test", iou_avg, sync_dist=True)
         self.test_step_outputs.clear()
 
-    def calc_metrics(self, pred_batch, gt_batch) -> Dict[str, float]:
-        averaged_metrics = {}
-        for key, metric_fn in metrics_dict.items():
-            averaged_metrics[key] = metric_fn(pred_batch, gt_batch)
-        return averaged_metrics
+    # def calc_metrics(self, pred_batch, gt_batch) -> Dict[str, float]:
+    #     averaged_metrics = {}
+    #     for key, metric_fn in metrics_dict.items():
+    #         averaged_metrics[key] = metric_fn(pred_batch, gt_batch)
+    #     return averaged_metrics
--- reconsthdr/lossfn_optimizer.py
+++ reconsthdr/lossfn_optimizer.py
@@ -1,15 +1,9 @@
 from typing import Literal
 
-import torch.nn as nn
+import torch
 import torch.optim as optim
 
-LossFunctionType = Literal["cross_entropy"]
 OptimizerType = Literal["sgd", "adam"]
-
-
-def loss_function_factory(loss_function_name: LossFunctionType):
-    if loss_function_name == "cross_entropy":
-        return nn.CrossEntropyLoss()
 
 
 def optimizer_factory(optimizer_name: OptimizerType, learning_rate: float, net):
@@ -17,3 +11,21 @@ def optimizer_factory(optimizer_name: OptimizerType, learning_rate: float, net):
         return optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
     elif optimizer_name == "adam":
         return optim.Adam(net.parameters(), lr=learning_rate)
+
+def loss_function_factory(loss_function_name: str):
+    if loss_function_name == "scale_invariant_mse":
+        return scale_invariant_mse_loss
+
+def scale_invariant_mse_loss(pred, gt, eps=1e-6):
+    """Scale invariant MSE loss
+    Args:
+        pred (torch.Tensor): predicted image
+        gt (torch.Tensor): ground truth image
+        eps (float, optional): epsilon value. Defaults to 1e-6.
+    Returns:
+        torch.Tensor: scale invariant MSE loss
+    """
+    return torch.mean(
+        torch.pow(torch.log(pred + eps) - torch.log(gt + eps), 2)
+        - torch.mean(torch.log(pred + eps) - torch.log(gt + eps)) ** 2
+    )
