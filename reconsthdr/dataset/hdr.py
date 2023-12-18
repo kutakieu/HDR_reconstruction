fb5df9b37c7fe61dad6b592a7684d129faeb595b
kutakieu <kutakieu22@gmail.com>
2023-12-18 22:11:10 +0900

    add hue jitter

--- reconsthdr/dataset/hdr.py
+++ reconsthdr/dataset/hdr.py
@@ -2,14 +2,14 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import List, Tuple
 
-import cv2
 import numpy as np
-from torchvision.transforms import ToTensor
+from torchvision.transforms.v2 import Compose, Normalize, ToTensor
 
 from . import BaseDataset
-from .augmentation import flip, random_rotate
+from .augmentation import apply_hue_jitter, flip, random_rotate
 from .tone_mappers import (BaseTMO, Drago, Durand, Mantiuk, PercentileExposure,
                            Reinhard)
+from .utils import load_hdr
 
 TMOs = [
     PercentileExposure,
@@ -34,24 +34,27 @@ class PanoHdrDataset(BaseDataset):
             for hdr_file in self.data_dir.glob(f"*.{fmt}"):
                 for tmo in TMOs:
                     self.data_samples.append(HdrDataSample(hdr_file, tmo))
+
+        self.img_transform = Compose([
+            ToTensor(),
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
     
     def __len__(self):
         return len(self.data_samples)
 
     def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray]:
-        hdr_img = cv2.cvtColor(cv2.imread(
-            self.data_samples[idx].hdr_file, flags=cv2.IMREAD_ANYDEPTH + cv2.IMREAD_COLOR
-        ), cv2.COLOR_BGR2RGB)
+        hdr_img = load_hdr(self.data_samples[idx].hdr_file)
         if self.is_training:
             hdr_img = self.augment(hdr_img)
-        ldr_img = self.data_samples[idx].tmo(hdr_img)
-        return ToTensor(hdr_img), ToTensor(ldr_img)
+        ldr_img = self.data_samples[idx].tmo(randomize=True)(hdr_img)
+        return ToTensor(hdr_img), self.img_transform(ldr_img)
     
     def augment(self, hdr_img: np.ndarray):
         if self.flip and np.random.rand() < 0.5:
             hdr_img = flip(hdr_img)
         if self.rotate:
             hdr_img = random_rotate(hdr_img)
+        if self.color_jitter:
+            hdr_img = apply_hue_jitter(hdr_img)
         return hdr_img
-
-
