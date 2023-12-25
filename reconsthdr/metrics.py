751ee65777b025b3027470a8f521a75e065ed652
kutakieu <kutakieu22@gmail.com>
2023-12-25 23:15:54 +0900

    add l1 metrics

--- reconsthdr/metrics.py
+++ reconsthdr/metrics.py
@@ -5,10 +5,12 @@ import numpy as np
 L1_ERROR_KEY = "L1_error"
 
 def calc_metrics(net_output: np.ndarray, batch: np.ndarray) -> List[float]:
-    metrics = {}
+    return {
+        L1_ERROR_KEY: l1_error
+    }
 
 def l1_error(pred: np.ndarray, gt: np.ndarray) -> float:
-    return np.abs(pred - gt).mean()
+    return np.mean(np.abs(pred - gt))
 
 metrics_dict = {
     L1_ERROR_KEY: l1_error
