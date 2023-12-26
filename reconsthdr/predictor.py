eca6c6b897e07280c61d5c836f70e6aa2d6afdf4
kutakieu <kutakieu22@gmail.com>
2023-12-26 23:01:11 +0900

    add predictor

---
+++ reconsthdr/predictor.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+import torch
+from omegaconf import DictConfig, OmegaConf
+from torchvision.transforms.v2 import Compose, Normalize, ToTensor
+
+from reconsthdr.lightning_wrapper import LightningHdrEstimator
+from reconsthdr.models import model_factory
+
+
+class Predictor:
+    def __init__(self, cfg: Union[str, DictConfig], weight_file: Union[str, Path], device="cpu") -> None:
+        if isinstance(cfg, str):
+            cfg = OmegaConf.load(cfg)
+        self.device = device
+        self.net = model_factory(cfg)
+        self.net.to(self.device)
+        if Path(weight_file).suffix == ".ckpt":
+            checkpoint = torch.load(weight_file)
+            lightning_model = LightningHdrEstimator(cfg)
+            lightning_model.load_state_dict(checkpoint['state_dict'])
+            self.net = lightning_model.net
+        else:
+            self.net.load_state_dict(torch.load(weight_file)["state_dict"])
+        self.net.eval()
+
+        self.img_transform = Compose([
+            ToTensor(),
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+
+    def __call__(self, ldr: np.ndarray) -> np.ndarray:
+        ldr = self.img_transform(ldr)
+        with torch.no_grad():
+            ldr = ldr.unsqueeze(0).to(self.device)
+            return self.net(ldr).cpu().permute(0, 2, 3, 1).numpy()[0]
