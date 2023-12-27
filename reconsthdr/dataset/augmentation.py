9eca3748c05f05070ff769b7309ce8c5ee4b762d
kutakieu <kutakieu22@gmail.com>
2023-12-27 20:03:18 +0900

    add scaling, wip color jitter

--- reconsthdr/dataset/augmentation.py
+++ reconsthdr/dataset/augmentation.py
@@ -1,9 +1,17 @@
-from random import random
+from random import random, uniform
 from typing import Optional, Tuple
 
+import cv2
 import numpy as np
-from torch import Tensor
-from torchvision.transforms.v2 import ColorJitter
+
+
+def random_scale(img: np.ndarray, img2: Optional[np.ndarray]):
+    scale = uniform(0.5, 2)
+    h, w = img.shape[:2]
+    if img2 is None:
+        return cv2.resize(img, (w*scale, h*scale))
+    h2, w2 = img2.shape[:2]
+    return cv2.resize(img, (w*scale, h*scale)), cv2.resize(img2, (w2*scale, h2*scale))
 
 
 def random_crop(img: np.ndarray, img2: Optional[np.ndarray], crop_hw: Tuple[int, int]):
@@ -50,16 +58,23 @@ def random_rotate(img: np.ndarray, img2: Optional[np.ndarray]) -> np.ndarray:
         return np.roll(img, rotate_pixels, axis=1)
     return np.roll(img, rotate_pixels, axis=1), np.roll(img2, rotate_pixels, axis=1)
 
-def apply_hue_jitter(img: np.ndarray, img2: Optional[np.ndarray], color_jitter: Optional[ColorJitter]=None):
+def apply_hue_jitter(img: np.ndarray, img2: Optional[np.ndarray], hue_jitter: float=0.05):
     """Apply color jitter to image
     Args:
         img (np.ndarray): input image
-        color_jitter (ColorJitter): color jitter transform
+        img2 Optional(np.ndarray): input image
     Returns:
         np.ndarray: color jittered image
     """
-    color_jitter = color_jitter or ColorJitter(hue=0.1)
+    img = img.copy()
+    hue_jitter = np.random.randint(-360*hue_jitter, 360*hue_jitter)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    img[:,:,0] = (img[:,:,0] + hue_jitter) % 360.0
+    img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
     if img2 is None:
-        return color_jitter(Tensor(img, device="cpu").permute(2, 0, 1)).permute(1, 2, 0).numpy()
-    return color_jitter(Tensor(img, device="cpu").permute(2, 0, 1)).permute(1, 2, 0).numpy(), \
-        color_jitter(Tensor(img2, device="cpu").permute(2, 0, 1)).permute(1, 2, 0).numpy()
+        return img
+    img2 = img2.copy()
+    img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2HSV)
+    img2[:,:,0] = (img2[:,:,0] + hue_jitter) % 360.0
+    img2 = cv2.cvtColor(img2, cv2.COLOR_HSV2RGB)
+    return img, img2
