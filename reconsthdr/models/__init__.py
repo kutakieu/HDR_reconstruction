fe4a43a345189663550786112bcda7540a79dd94
kutakieu <kutakieu22@gmail.com>
2023-12-31 12:31:44 +0900

    rename

--- reconsthdr/models/__init__.py
+++ reconsthdr/models/__init__.py
@@ -5,7 +5,7 @@ from .unet import Unet
 def model_factory(cfg):
     if cfg.model.name == "expandnet":
         return ExpandNet()
-    elif cfg.model.name == "unet":
+    elif cfg.model.name == "hdr_unet":
         return Unet()
     else:
         raise NotImplementedError
